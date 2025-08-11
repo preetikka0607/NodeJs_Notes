835ef6ce5a8e3feb36adf76d7f579c245cf9f98f
Preetikka <spreetikka@gamil.com>
2025-08-11 21:25:09 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Notes
+//Node Js - is a Javascript Runtime Environment which runs outside the browser
+//Node Js => V8 (Google's Javascript Engine) + libuv
+//asynchronous single threaded (runs on single thread but asynchronous)
+//this is why node js is fast (NON Blocking I/O Calls)
+
+const var1 = "Hello";
+const var2 = 1+2 + "Hello" + (3 + 4 +5);
+console.log(var1);
+console.log(var2);
+
